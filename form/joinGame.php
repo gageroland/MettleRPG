e6e7113c89d5e45e7f106615f9cadd4fc992ca80
wulfCode <gageroland@gmail.com>
2017-10-18 13:58:49 +0300

    8/6/17 Project
    Reupload from another repository.

---
+++ form/joinGame.php
@@ -0,0 +1,29 @@
+<?php
+	//Database Information
+	$hostname="1ad987f704ab:3306";
+	$username="root";
+	$password="weouthere1";
+	$dbname="sourcelair";
+
+	//Connect to Database
+	mysql_connect("$hostname","$username", "$password") or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please email contact@wulf.design'),history.go(-1)</script></html>");
+	mysql_select_db("$dbname");
+
+	//Begin the session for the user
+	session_start();
+
+	//Get the values from the Join Game form using POST method
+	$the_name=$_POST['campaign_num'];
+	$the_userRole=$_POST['j_role'];
+
+	//Perform SQL command
+	$select = "insert into gamePlayers (j_userName, j_gameNumber, j_userType) values 
+    ('".$_SESSION["USERNAME"]."', '".$the_name."', '".$the_userRole."')";
+	$sql=mysql_query($select);
+
+	//Close the connection
+	mysql_close();
+
+	//Direct the user back to the same page (refresh page)
+	header('Location: /my-games/select.php');
+?>
---
+++ my-games/template.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/my-games.css">
+		<title>GameTitle</title>
+		<script src="../scripts/journal.js"></script>
+    </head>
+    <body>
+        <!-- Start the session for the user -->
+        <?php session_start();?>
+        <div class="navBar">
+            <a class="rightMost" href="/index.php">Logout</a>
+            <a href="/my-games/select.php">Dashboard</a>
+			<?php echo "<p>".$_SESSION["USERNAME"]."</p>"?>
+        </div>
+		<div id="outerDiv">
+			<div id="journalDiv">
+				<!-- Load the HTML templates into this object -->
+                <?php
+					//Database Information
+    				$hostname="1ad987f704ab:3306";
+					$username="root";
+					$password="weouthere1";
+					$dbname="sourcelair";
+
+            		//Connect to Database
+        			$conn = mysql_connect("$hostname","$username", "$password");
+					mysql_select_db($dbname);
+
+            		//Perform SQL command
+                	$strSQL = "SELECT * FROM items WHERE item_id = 3 ";
+        			$rs = mysql_query($strSQL);
+
+            		//Print the HTML from the data in the Character Creation table
+             		while($row = mysql_fetch_array($rs))
+                    {
+        				echo $row["data"];
+    				}
+                ?>
+			</div>
+            <!-- Update the HTML -->
+			<button onclick="updateInnerHTML('journalDiv')">Save</button>
+		</div>
+    </body>
+</html>
